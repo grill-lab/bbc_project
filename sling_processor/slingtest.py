4bb3e020de2493c0d60027d2b5fd5a9a4c525bb8
Default Application User <zayyargla>
2021-05-11 10:38:10 +0000

    remodified

--- sling_processor/slingtest.py
+++ sling_processor/slingtest.py
@@ -1,109 +1,130 @@
 import sling
 import pprint
-import requests
-import pprint
 import json
+import sys
+import argparse
+import os
 
-
-def print_frame(frame):
-    print(frame.get("name"))
-    print(frame.get("/w/item/category").get("/w/item/category"))
-    for key, value in frame:
-        key_is_frame = type(key) is sling.Frame
-        description = key
-        if key_is_frame:
-            if "name" in key:
-                description = key.get("name")
-            elif "id" in key:
-                description = key.get("id")
-            else:
-                pass
-                
-        value_is_frame = type(value) is sling.Frame
-        value_name = value
-        if value_is_frame:
-            if "name" in value:
-                value_name = value.get("name")
-            else:
-                temp = ""
-                for k, v in value:
-                    inner_key = k
-                    if type(k) is sling.Frame:
-                        if "name" in k:
-                            inner_key = k.get("name")
-                    inner_value = v
-                    if type(v) is sling.Frame:
-                        if "name" in v:
-                            inner_value = v.get("name")
-                    temp += f"{(inner_key, inner_value)} | "
-                value_name = temp
-        print(f"{type(description)} / {(description, value_name)} / {type(value) is sling.Frame}")
-        print("_________________________________________________________________________")
-
-
-def get_string_from_sling_object(item):
-    if "name" in item:
-        return item.get("name")
-    elif "id" in item:
-        return item.get("id")
-    elif "is" in item:
-        return item.get("is")
+def get_string_name_from_sling_object(item):
+    if type(item) is sling.Frame:
+        if "name" in item:
+            return item.get("name")
+        elif "is" in item:
+            return item.get("is")
+        elif "id" in item:
+            return item.get("id")
     else:
-        return ""
+        return item
     
 def check_if_frame_wikidata_item(item):
     if "isa" in item:
-        return get_string_from_sling_object(item.get("isa")) == "Wikidata item"
+        return get_string_name_from_sling_object(item.get("isa")) == "Wikidata item"
     return False
-            
+
+def check_if_frame_wikidata_item_2(item):
+    if "name" in item:
+        return True
+    return False
     
-def frame_to_trecweb(frame):
+def frame_to_json(frame):
     output_json = {"name": "", "category": [], "related_entities": [], "attributes": [], "contents": "", "id": ""}
+    name = frame.name
+    if type(name) is not str:
+        return {}
+    output_json["name"] = frame.name
+    output_json["id"] = frame.id
+    
     for key, value in frame:
-        key_is_frame = type(key) is sling.Frame
-        key_description = get_string_from_sling_object(key)
+        key_description = get_string_name_from_sling_object(key)
+        if key_description == "/w/item/category":
+            output_json["category"].append(get_string_name_from_sling_object(value))
+            continue
         
-        if key_description == "name":
-            if type(key_description) is bytes:
-                continue
-            output_json["name"] = str(value)
-        elif key_description == "id":
-            output_json["id"] = str(value)
-        elif key_description == "/w/item/category":
-            output_json["category"].append(str(get_string_from_sling_object(value)))
-        else:
-            value_description = value
-            if type(value_description) is sling.Frame:
-                is_wikidata_item = check_if_frame_wikidata_item(value_description)        
-                value_description = get_string_from_sling_object(value)
-                
-                if is_wikidata_item and value_description != "" and type(value_description) is str:
-                    output_json["related_entities"].append(value_description)
-                    
-                if type(value_description) is sling.Frame:
-                    is_wikidata_item = check_if_frame_wikidata_item(value_description)        
-                    value_description = get_string_from_sling_object(value_description)
-                    if is_wikidata_item and value_description != "" and type(value_description) is str:
-                        output_json["related_entities"].append(value_description)
-                        
-                if type(value_description) is str:
-                    output_json["attributes"].append([key_description, value_description])
+        value_description = value
+        if type(value_description) == str:
+            output_json["attributes"].append([key_description, value])
+            
+        elif type(value_description) is sling.Frame:
+            value_description = get_string_name_from_sling_object(value)
+            
+            if type(value_description) is str:
+                output_json["attributes"].append([key_description, value_description])
+            elif type(value_description) is sling.Frame:
+                if check_if_frame_wikidata_item_2(value_description):
+                    output_json["related_entities"].append(value_description.name)
+                else:
+                    for k, v in value_description:                            
+                        inner_k = get_string_name_from_sling_object(k)
+                        inner_v = get_string_name_from_sling_object(v)
+                        if type(inner_k) is str and type(inner_v) is str:
+                            output_json["attributes"].append([key_description, [inner_k, inner_v]])
+                        if type(v) is sling.Frame:
+                            if check_if_frame_wikidata_item_2(v):
+                                output_json["related_entities"].append(v.name)
             else:
-                if type(value) is str:
-                    output_json["attributes"].append([key_description, value])
-                    
+                pass
+        else:
+            pass
+                
     for predicate, objects in output_json["attributes"]:
         output_json["contents"] += f"{predicate} {objects} "
     return output_json
         
 if __name__ == "__main__":
-
-    kb = sling.Store()
-    kb.load("/nfs/project/bbc/sling/wiki/kb.sling")
-    names = sling.PhraseTable(kb, "/nfs/project/bbc/sling/wiki/en/phrase-table.repo")
-    n_item = kb["/w/item"]
-    kb.freeze()
     
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output_folder')
+    parser.add_argument('kb_sling_filepath', default = "./wiki/kb.sling")
+    parser.add_argument('phrase_table_filepath', default = "./wiki/en/phrase-table.repo")
+    args = parser.parse_args()
+    
+    kb_sling_filepath = args.kb_sling_filepath
+    phrase_table_filepath = args.phrase_table_filepath
+    output_folder = args.output_folder
+    
+    kb = sling.Store()
+    kb.load(kb_sling_filepath)
+    names = sling.PhraseTable(kb, phrase_table_filepath)
+    n_item = kb["/w/item"]
+    kb_len = len(kb)
+    kb.freeze()
+   
+    x = 0
+    i = 0
+    h = open(os.path.join(output_folder, f"kb_index_{x}.jsonl"), "w+")
+    for f in kb:
+        if f.isa(n_item):
+            try:
+                json_frame = frame_to_json(f)
+                if len(json_frame) > 0:
+                    h.write(json.dumps(json_frame))
+                    h.write("\n")
+                    i += 1
+                if i % 10000 == 0:
+                    print(f"DONE: {i} / {kb_len}")
+                if i % 10000000 == 0 or i >= kb_len:
+                    h.close()
+                    x += 1
+                    h = open(f"./index_collections/index_collection_3/kb_index_{x}.json", "w+")
+            except Exception as e:
+                print(f)
+                print(e)
+                print("___________________________________")
+                
+            
+                    
+    
+ '''
+    for entity in names.lookup("Glasgow"):
+        x = frame_to_json(entity)
+        with open("./testjson.json", "w") as test_file:
+            json.dump(x, test_file) 
+
+        break
+    '''
+    '''
+    
+
     output_list = []
     kb_len = len(kb)
     x = 0
@@ -111,13 +132,13 @@ if __name__ == "__main__":
     for f in kb: 
         if f.isa(n_item):
             try:
-                json_frame = frame_to_trecweb(f
+                json_frame = frame_to_json(f)
                 output_list.append(json_frame)
                 i += 1
                 if i % 10000 == 0:
                     print(f"DONE: {i} / {kb_len}")
                 if i % 10000000 == 0 or i >= kb_len:
-                    with open(f"./index_collection/kb_index_{x}.json", "w+") as g:
+                    with open(f"./index_collections/index_collection_3/kb_index_{x}.json", "w+") as g:
                         json.dump(output_list, g)
                     output_list = []
                     x += 1
@@ -130,10 +151,7 @@ if __name__ == "__main__":
         g.close()
     except:
         pass
-
-    
-    
-
+    '''
 
 
     
